09a2daf4252b4c3afdc2128a65a4cdfc02cc288d
yomisana <sammywhy3236@gmail.com>
2024-07-30 23:01:01 +0800

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // This base Path will be at
+  // https://<username>.github.io/yomisana-nextjs-template
+  basePath: "/yomisana-nextjs-template",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default withNextIntl(nextConfig);
