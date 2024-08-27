6f70f17628a6e8d2676a8266cb91d810977d0e98
優米撒那 <93663341+Yomisana@users.noreply.github.com>
2024-08-27 20:06:53 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/nextjs-i18n-template",
   trailingSlash: true,
   reactStrictMode: true,
 };
