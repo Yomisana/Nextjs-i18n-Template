29fe87f89af1cf1dd169f3cf98fd160635d575c5
優米撒那 <93663341+Yomisana@users.noreply.github.com>
2024-08-27 20:01:56 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // <=== enables static exports
   trailingSlash: true,
   reactStrictMode: true,
 };
